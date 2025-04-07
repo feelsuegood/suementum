a6fddd29d972c4cc003bf3a1dcba76f827025814
feelsuegood <suaj2021@gmail.com>
2025-04-08 01:27:22 +1000

    suementum styling

--- js/weather.js
+++ js/weather.js
@@ -1,20 +1,19 @@
-const API_KEY = process.env.WEATHER_API_KEY;
-
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url = `/api/weather?lat=${lat}&lon=${lon}`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       const city = document.querySelector("#weather span:last-child");
       const weather = document.querySelector("#weather span:first-child");
       city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
     });
 }
+
 function onGeoError() {
-  alert("Cant' find you. No weather for you.");
+  alert("Can't find you. No weather for you.");
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
