dc96c542f60d0e78818bed84fe404e4afdcb5bab
feelsuegood <suaj2021@gmail.com>
2025-04-09 09:25:18 +1000

    update css

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,9 @@
+const API_KEY = "baf4662acc6781377cd134c2ce29989c";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `/api/weather?lat=${lat}&lon=${lon}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
